9b8ee8870548ac07956ed0e04fc64c4d82df9e7b
sierra403 <btlee94@gmail.com>
2022-11-29 08:03:07 -0800

    switch to a postgres db

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,11 +1,15 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = ({ env }) => ({
   connection: {
-    client: 'sqlite',
+    client: "postgres",
     connection: {
-      filename: path.join(__dirname, '..', env('DATABASE_FILENAME', '.tmp/data.db')),
+      host: env("DATABASE_HOST", "127.0.0.1"),
+      port: env.int("DATABASE_PORT", 5432),
+      database: env("DATABASE_NAME", "strapi_template"),
+      user: env("DATABASE_USERNAME", "postgres"),
+      password: env("DATABASE_PASSWORD", "password"),
     },
-    useNullAsDefault: true,
+    debug: false,
   },
 });
