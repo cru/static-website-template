3473b9dac8e786bba87235345c65675eb559f200
Brandon Lee <38223263+sierra403@users.noreply.github.com>
2023-09-08 13:53:02 -0600

    Stories section for testimonials

--- phoenix/src/components/Stories.js
+++ phoenix/src/components/Stories.js
@@ -1,86 +1,86 @@
-import React, { useEffect, useState } from 'react'
 import Slider from 'react-slick'
-import { Col, Row } from 'antd/lib/grid'
+import Image from 'next/image'
 
-import { shuffle } from '../utils'
+import hero2 from 'public/images/hero2.jpg'
 
-export const Stories = (props) => {
-  const [stories, setStories] = useState([])
-
-  useEffect(() => {
-    const items = [
-      <div key={'1'}>
-        <h4>Patient 1, 2016-2017</h4>
-        <h5>University of Calgary, Canada</h5>
-        <br />
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc mattis non nibh in
-          ultrices. Orci varius natoque penatibus et magnis dis parturient montes,
-          nascetur ridiculus mus. Praesent ullamcorper porta urna quis rhoncus. Duis
-          commodo tellus dictum orci ultricies elementum. Mauris pellentesque molestie
-          velit, nec venenatis lacus mollis sed. Suspendisse et mauris eu erat
-          pellentesque consectetur. Morbi in gravida nisi. Curabitur et facilisis diam,
-          vel congue ex. Maecenas quis mauris porttitor, ullamcorper mi vel, lacinia ante.
-          Phasellus quis imperdiet nisl, finibus tempor lectus. Mauris at lacinia quam, ac
-          efficitur velit. Etiam ut condimentum ex. Suspendisse fermentum odio vel magna
-          pharetra ornare.
-        </p>
-      </div>,
-      <div key={'2'}>
-        <h4>Patient 2, 2015-2016</h4>
-        <h5>University of Alberta, Canada</h5>
-        <br />
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed mi dolor, vehicula
-          ac gravida ut, pulvinar ac orci. Nulla a sollicitudin lorem. In ac lacus auctor,
-          aliquam ex non, mollis erat. Mauris finibus lorem sed tellus consectetur,
-          lacinia aliquet diam.
-        </p>
-      </div>,
-      <div key={'3'}>
-        <h4>Patient 3, 2019-2020</h4>
-        <h5>Foothills Medical Center, Canada</h5>
-        <br />
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer suscipit
-          convallis sollicitudin. Duis in diam vehicula, venenatis sapien vel, ullamcorper
-          neque. Nam placerat feugiat odio ut condimentum. Proin dignissim nisi et elit
-          elementum faucibus. Nullam malesuada ligula at venenatis eleifend. Praesent non
-          dui eget tellus consectetur molestie. Ut pulvinar vehicula risus, et rutrum quam
-          ultrices non. Curabitur hendrerit lectus id leo scelerisque, nec varius felis.
-        </p>
-      </div>,
-    ]
-
-    setStories(shuffle(items))
-  }, [])
-
+const Stories = ({ forwardRef }) => {
   const settings = {
-    dots: true,
     arrows: false,
+    dots: true,
     infinite: true,
+    autoplay: true,
     slidesToShow: 1,
     slidesToScroll: 1,
-    autoplay: true,
-    speed: 1500,
+    speed: 1000,
     autoplaySpeed: 10000,
-    fade: true,
   }
 
   return (
-    <div ref={props.storiesRef} className='section section-light-right'>
-      <Row align='middle' className='align-middle container'>
-        <Col span={12}>
-          <picture>
-            <source srcSet={'/images/banner_2.webp'} type='image/webp' />
-            <img className='hero-img' src={'/images/banner_2.jpg'} alt='banner' />
-          </picture>
-        </Col>
-        <Col span={12} style={{ paddingLeft: '3rem' }}>
+    <div
+      ref={forwardRef}
+      className='w-full p-8 lg:h-content lg:h-min-[525px] lg:bg-gradient-to-r lg:from-white lg:from-45% lg:to-gray-100 lg:to-45% h-content h-min-[800px] bg-gradient-to-b from-white from-40% to-gray-100 to-40%'
+    >
+      <div className='grid grid-cols-2 gap-8 items-center container layout h-full lg:grid-cols-2 lg:justify-items-start justify-items-center'>
+        <div className='relative w-full h-[400px] overflow-hidden shadow-2xl'>
+          <Image
+            src={hero2}
+            alt='empty'
+            fill
+            placeholder='blur'
+            className='object-cover '
+          />
+        </div>
+        <div className='w-full'>
           <h1>Patient Stories</h1>
+          <hr />
           <Slider {...settings}>{stories}</Slider>
-        </Col>
-      </Row>
+        </div>
+      </div>
     </div>
   )
 }
+
+const stories = [
+  <div key={'1'}>
+    <h4>Patient 1, 2016-2017</h4>
+    <h5>University of Calgary, Canada</h5>
+    <br />
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc mattis non nibh in
+      ultrices. Orci varius natoque penatibus et magnis dis parturient montes, nascetur
+      ridiculus mus. Praesent ullamcorper porta urna quis rhoncus. Duis commodo tellus
+      dictum orci ultricies elementum. Mauris pellentesque molestie velit, nec venenatis
+      lacus mollis sed. Suspendisse et mauris eu erat pellentesque consectetur. Morbi in
+      gravida nisi. Curabitur et facilisis diam, vel congue ex. Maecenas quis mauris
+      porttitor, ullamcorper mi vel, lacinia ante. Phasellus quis imperdiet nisl, finibus
+      tempor lectus. Mauris at lacinia quam, ac efficitur velit. Etiam ut condimentum ex.
+      Suspendisse fermentum odio vel magna pharetra ornare.
+    </p>
+  </div>,
+  <div key={'2'}>
+    <h4>Patient 2, 2015-2016</h4>
+    <h5>University of Alberta, Canada</h5>
+    <br />
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed mi dolor, vehicula ac
+      gravida ut, pulvinar ac orci. Nulla a sollicitudin lorem. In ac lacus auctor,
+      aliquam ex non, mollis erat. Mauris finibus lorem sed tellus consectetur, lacinia
+      aliquet diam.
+    </p>
+  </div>,
+  <div key={'3'}>
+    <h4>Patient 3, 2019-2020</h4>
+    <h5>Foothills Medical Center, Canada</h5>
+    <br />
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer suscipit convallis
+      sollicitudin. Duis in diam vehicula, venenatis sapien vel, ullamcorper neque. Nam
+      placerat feugiat odio ut condimentum. Proin dignissim nisi et elit elementum
+      faucibus. Nullam malesuada ligula at venenatis eleifend. Praesent non dui eget
+      tellus consectetur molestie. Ut pulvinar vehicula risus, et rutrum quam ultrices
+      non. Curabitur hendrerit lectus id leo scelerisque, nec varius felis.
+    </p>
+  </div>,
+]
+
+export default Stories
--- phoenix/src/pages/index.js
+++ phoenix/src/pages/index.js
@@ -6,6 +6,8 @@ import Donate from 'src/components/Donate'
 import Footer from 'src/components/Footer'
 import Mission from 'src/components/Mission'
 import Services from 'src/components/Services'
+import Socials from 'src/components/Socials'
+import Stories from 'src/components/Stories'
 import constants from 'src/constants'
 import api from 'src/lib/api'
 
@@ -58,41 +60,16 @@ const Home = () => {
   }
 
   return (
-    <div className='space-y-16'>
+    <div className='space-y-32'>
       {/* <Header scrollTo={scrollTo} /> */}
       <Banner />
       <Services scrollRef={aboutRef} />
       <Mission scrollRef={missionRef} />
       <Contact scrollRef={contactRef} />
       {/* <Team teamRef={teamRef} /> */}
+      <Stories />
       <Donate donateRef={donateRef} />
-      {/* <div className='section-light-thin'>
-              <Row  align='middle' justify='center' gutter={32}>
-                <Col xl={24} lg={24} md={24} sm={24} xs={24}>
-                  <h5>Extra info or social media links</h5>
-                </Col>
-                <Col>
-                  <a
-                    className='btn-header-link'
-                    href='https://cru.ucalgary.ca/'
-                    target='_blank'
-                    rel='noopener noreferrer'
-                  >
-                    <Facebook />
-                  </a>
-                </Col>
-                <Col>
-                  <a
-                    className='btn-header-link'
-                    href='https://cru.ucalgary.ca/'
-                    target='_blank'
-                    rel='noopener noreferrer'
-                  >
-                    <Instagram />
-                  </a>
-                </Col>
-              </Row>
-            </div> */}
+      <Socials />
       <Footer />
     </div>
   )
