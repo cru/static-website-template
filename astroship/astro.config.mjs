a5996bbaaca9d5a2507d150f17c251549c6cc99d
Brandon Lee <38223263+sierra403@users.noreply.github.com>
2024-04-15 09:47:49 -0600

    lib updates: astroship

--- astroship/astro.config.mjs
+++ astroship/astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
@@ -10,5 +11,6 @@ export default defineConfig({
     tailwind(),
     mdx(),
     sitemap(),
+    icon()
   ],
 });
